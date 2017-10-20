4dce39c1c955e96a30013de2ae79aef3365bca6a
Chi Voong <chi.voong@gmail.com>
2017-10-20 15:37:39 +0100

    added dummy test to project

---
+++ src/test/java/postmybuild/controller/TestController.java
@@ -0,0 +1,12 @@
+package postmybuild.controller;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestController {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
