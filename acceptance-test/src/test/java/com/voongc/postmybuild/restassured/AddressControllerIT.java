a5a8e9f5e0f87f3cb1363a2430e0c516c00b460d
Chi Voong <chi.voong@gmail.com>
2024-02-21 10:39:32 +0000

    Html report (#13)
    * surefire html report from maven-site-plugin

--- acceptance-test/src/test/java/com/voongc/postmybuild/restassured/AddressControllerIT.java
+++ acceptance-test/src/test/java/com/voongc/postmybuild/restassured/AddressControllerIT.java
@@ -1,5 +1,7 @@
 package com.voongc.postmybuild.restassured;
 
+import com.voongc.postmybuild.entity.Address;
+import com.voongc.postmybuild.entity.Builder;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
@@ -7,16 +9,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import com.voongc.postmybuild.entity.Builder;
-import com.voongc.postmybuild.entity.Address;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class BuilderControllerTest {
+
+public class AddressControllerIT {
     static Builder createdBuilder = null;
+    static Long builderId = 0L;
+    static Long addressId = 0L;
     @BeforeAll
     public static void beforeAll(){
         // Check if the environment variable is set
@@ -30,9 +33,8 @@ public class BuilderControllerTest {
 
         Address address = new Address("1", "Data St", "West Yorks", "UK", "YO1 3AB");
         // Create a sample Builder object
-        Builder builder = new Builder("Before Builders Ltd", "John", "Smith", List.of(address));
-
-        // Perform a POST request and validate the response
+        Builder builder = new Builder("John", "Doe", "Smith", List.of(address));
+        // Perform a POST request and add some data
         createdBuilder = given()
                 .contentType(ContentType.JSON)
                 .body(builder)
@@ -40,50 +42,50 @@ public class BuilderControllerTest {
                 .post("/builder/createBuilder")
                 .then()
                 .statusCode(HttpStatus.SC_CREATED)
-                .body("name", equalTo("Before Builders Ltd"))
-                .body("forename", equalTo("John"))
+                .body("name", equalTo("John"))
+                .body("forename", equalTo("Doe"))
                 .body("surname", equalTo("Smith"))
                 .log()
                 .all()
                 .extract().response().as(Builder.class);
+
+        Assertions.assertEquals(1,createdBuilder.getAddresses().size());
+
+        builderId = createdBuilder.getId();
+        addressId = createdBuilder.getAddresses().get(0).getId();
     }
-
     @Test
-    public void testCreateBuilder() {
-        Address address = new Address("1", "Data St", "West Yorks", "UK", "YO1 3AB");
-        // Create a sample Builder object
-        Builder builder = new Builder("Builders Inc.", "Chris", "Steward", List.of(address));
-
-        // Perform a POST request and validate the response
-        given()
-            .contentType(ContentType.JSON)
-            .body(builder)
-        .when()
-            .post("/builder/createBuilder")
-        .then()
-            .statusCode(HttpStatus.SC_CREATED)
-            .body("name", equalTo("Builders Inc."))
-            .body("forename", equalTo("Chris"))
-            .body("surname", equalTo("Steward"))
-                .log().all();
-    }
-
-    @Test
-    public void testGetBuilder(){
-        Builder fetchedBuilder = given()
-                .get("/builder/" + createdBuilder.getId())
+    public void testAddAddress(){
+        Address existingAddress = createdBuilder.getAddresses().get(0);
+        Address newAddress = new Address("1","New St","West Yorks","UK","LS11TT");
+        Builder updatedBuilder = given()
+                .contentType(ContentType.JSON)
+                .body(newAddress)
+                .when()
+                .post("/builder/" + builderId + "/addAddress")
                 .then()
                 .statusCode(HttpStatus.SC_OK)
                 .log()
                 .all()
                 .extract().response().as(Builder.class);
 
-        Assertions.assertEquals(createdBuilder,fetchedBuilder);
+        Assertions.assertEquals(2,updatedBuilder.getAddresses().size());
+        Assertions.assertEquals(existingAddress.getHouseNo(),updatedBuilder.getAddresses().get(0).getHouseNo());
+        Assertions.assertEquals(existingAddress.getHouseNo(),updatedBuilder.getAddresses().get(0).getHouseNo());
+        Assertions.assertEquals(newAddress.getHouseNo(),updatedBuilder.getAddresses().get(1).getHouseNo());
+        Assertions.assertEquals(newAddress.getStreet(),updatedBuilder.getAddresses().get(1).getStreet());
+
     }
 
     @Disabled
     @Test
-    public void testUpdateBuilder(){
+    public void testUpdateAddress(){
+
     }
 
+    @Disabled
+    @Test
+    public void testGetAllAddresses(){
+
+    }
 }
